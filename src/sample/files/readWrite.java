d09df606a36556481094269fc916dd1bcfee61ee
Giridhar Shanbhag <geeks2022@gmail.com>
2020-10-01 20:10:03 +0530

    Initial Commit for PathFinderVisualizer

--- src/sample/files/readWrite.java
+++ src/sample/files/readWrite.java
@@ -14,7 +14,8 @@ import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class readWrite {
-    private readWrite() {}
+    private readWrite() {
+    }
 
     public static void save(String path) {
         try {
@@ -23,18 +24,19 @@ public class readWrite {
             StringBuilder sb = new StringBuilder();
             boolean possibleSave = false;
 
-            for(int i = 0; i < c_vals.ROW; i++) {
-                for(int j = 0; j < c_vals.COL; j++) {
-                    if(MazeController.Grid[i][j].state == cellState.WALL)
+            for (int i = 0; i < c_vals.ROW; i++) {
+                for (int j = 0; j < c_vals.COL; j++) {
+                    if (MazeController.Grid[i][j].state == cellState.WALL)
                         possibleSave = true;
 
                     sb.append(stateNum.getChar(MazeController.Grid[i][j].state));
                 }
-                if(i != c_vals.ROW-1) sb.append("\n");
+                if (i != c_vals.ROW - 1) sb.append("\n");
             }
 
-            if(!possibleSave) { // the maze is not saved, delete the file, and close the FileWriter
-                fw.close(); File file = new File(path);
+            if (!possibleSave) { // the maze is not saved, delete the file, and close the FileWriter
+                fw.close();
+                File file = new File(path);
                 file.delete(); // delete the file if the file is empty
 
                 return;
@@ -42,57 +44,55 @@ public class readWrite {
 
             fw.write(sb.toString());
             fw.close();
+        } catch (Exception ignored) {
         }
-        catch (Exception ignored) { }
     }
 
-    public static ArrayList<cell> read(String path)
-    {
+    public static ArrayList<cell> read(String path) {
         try {
             BufferedReader br = new BufferedReader(new FileReader(path));
             ArrayList<cell> wallAr = new ArrayList<>();
             cell curr;
 
-            for(int i = 0; i < c_vals.ROW; i++) {
+            for (int i = 0; i < c_vals.ROW; i++) {
                 String str = br.readLine();
-                for(int j = 0; j < c_vals.COL; j++) {
+                for (int j = 0; j < c_vals.COL; j++) {
 
                     curr = MazeController.Grid[i][j];
 
-                    if(curr.state != cellState.SOURCE && curr.state != cellState.TARGET) {
+                    if (curr.state != cellState.SOURCE && curr.state != cellState.TARGET) {
                         MazeController.PaintBlock(i, j, c_vals.BORDER, c_vals.UNVISITED);
 
                         curr.state = stateNum.getNum(str.charAt(j));
                     }
-                    if(curr.state == cellState.WALL) {
+                    if (curr.state == cellState.WALL) {
                         wallAr.add(new cell(i, j));
                     }
                 }
             }
             return wallAr;
+        } catch (Exception ignored) {
         }
-        catch (Exception ignored) { }
         return null;
     }
 
-    public static ArrayList<cell> loadRandomMaze()
-    {
+    public static ArrayList<cell> loadRandomMaze() {
         String maze = mazegen.buildMaze();
 
         ArrayList<cell> wallAr = new ArrayList<>();
         cell curr;
 
-        for(int i = 0; i < c_vals.ROW; i++) {
-            for(int j = 0; j < c_vals.COL; j++) {
+        for (int i = 0; i < c_vals.ROW; i++) {
+            for (int j = 0; j < c_vals.COL; j++) {
 
                 curr = MazeController.Grid[i][j];
 
-                if(curr.state != cellState.SOURCE && curr.state != cellState.TARGET) {
+                if (curr.state != cellState.SOURCE && curr.state != cellState.TARGET) {
                     MazeController.PaintBlock(i, j, c_vals.BORDER, c_vals.UNVISITED);
 
-                    curr.state = stateNum.getNum(maze.charAt(i*c_vals.ROW + j));
+                    curr.state = stateNum.getNum(maze.charAt(i * c_vals.ROW + j));
                 }
-                if(curr.state == cellState.WALL) {
+                if (curr.state == cellState.WALL) {
                     wallAr.add(new cell(i, j));
                 }
             }
--- src/sample/maze/mazegen.java
+++ src/sample/maze/mazegen.java
@@ -18,13 +18,14 @@ class Point {
 public class mazegen {
     private static Random random = new Random();
 
-    private static int[] Y = {-1,0,1, 0};
-    private static int[] X = { 0,1,0,-1};
+    private static int[] Y = {-1, 0, 1, 0};
+    private static int[] X = {0, 1, 0, -1};
 
     private static char[] color = {stateNum.getChar(cellState.WALL),
             stateNum.getChar(cellState.UNVISITED)};
 
-    private mazegen() { }
+    private mazegen() {
+    }
 
     private static boolean inRange(int r, int c) {
         return (r >= 0 && r < c_vals.ROW) && (c >= 0 && c < c_vals.COL);
@@ -47,40 +48,42 @@ public class mazegen {
         Point p = new Point(randint(c_vals.ROW), randint(c_vals.COL));
         st.push(p);
 
-        while(!st.isEmpty())
-        {
+        while (!st.isEmpty()) {
             curr = st.peek(); // get the top Point
 
             maze[curr.i][curr.j] = 1;
             ArrayList<Integer> nlst = new ArrayList<>();
 
-            for(int i = 0; i < 4; i++) {
-                ni = curr.i+Y[i]; nj = curr.j+X[i];
-                if(inRange(ni, nj)) {
-                    if(maze[ni][nj] == 0) {
+            for (int i = 0; i < 4; i++) {
+                ni = curr.i + Y[i];
+                nj = curr.j + X[i];
+                if (inRange(ni, nj)) {
+                    if (maze[ni][nj] == 0) {
                         // # of occupied neighbours must be 1
                         int ctr = 0;
-                        for(int j = 0; j < 4; j++) {
-                            ei = ni+Y[j]; ej = nj+X[j];
-                            if(inRange(ei,ej)) {
-                                if(maze[ei][ej] == 1) ctr += 1;
+                        for (int j = 0; j < 4; j++) {
+                            ei = ni + Y[j];
+                            ej = nj + X[j];
+                            if (inRange(ei, ej)) {
+                                if (maze[ei][ej] == 1) ctr += 1;
                             }
                         }
-                        if(ctr == 1) nlst.add(i);
+                        if (ctr == 1) nlst.add(i);
                     }
                 }
             }
 
-            if(nlst.size() > 0) {
+            if (nlst.size() > 0) {
                 int ranDir = nlst.get(randint(nlst.size()));
-                st.add(new Point(curr.i+Y[ranDir], curr.j+X[ranDir]));
+                st.add(new Point(curr.i + Y[ranDir], curr.j + X[ranDir]));
+            } else {
+                st.pop();
             }
-            else { st.pop(); }
         }
 
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < c_vals.ROW; i++) {
-            for(int j = 0; j < c_vals.COL; j++) {
+        for (int i = 0; i < c_vals.ROW; i++) {
+            for (int j = 0; j < c_vals.COL; j++) {
                 sb.append(color[maze[i][j]]);
             }
         }
